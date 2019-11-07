fb3ff8099a73905f4d495461ed3e18b2bd8f8c01
AndreaCoop <andrea.cupertino@gmail.com>
2019-11-07 16:11:33 +0100

    1st attemp using drawnow

--- video_generator_script.py
+++ video_generator_script.py
@@ -2,4 +2,32 @@
 
 # import data
 
-[X,Y] = np.loadtxt("test.txt")
+pip install drawnow
+
+# import packages
+import numpy as np
+import matplotlib as plt
+from drawnow import drawnow, figure
+
+# load data
+[X,Y] = np.loadtxt("testSin.txt")
+
+# function to generate plot
+def draw_fig_real(axes):
+    #figure() # don't call, otherwise opens new window
+    # plt.pyplot.imshow(z, interpolation='nearest')
+    #plt.colorbar()
+    #show()
+    axes1.plot(X1,Y1)
+
+# video
+N = 16
+fig, axes1 = plt.pyplot.subplots()
+axes1.set_xlim(0,9)
+for i in range(1,N):
+    j=i*(len(X)//N)
+    X1=X[0:j]
+    Y1=Y[0:j]
+    drawnow(draw_fig_real)
+
+
