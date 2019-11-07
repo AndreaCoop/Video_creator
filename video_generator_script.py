39d63370fac695db522f31212615931eb96847ad
AndreaCoop <andrea.cupertino@gmail.com>
2019-11-07 16:30:30 +0100

    script generates 16 frames

--- video_generator_script.py
+++ video_generator_script.py
@@ -6,28 +6,29 @@ pip install drawnow
 
 # import packages
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from drawnow import drawnow, figure
 
 # load data
 [X,Y] = np.loadtxt("testSin.txt")
 
-# function to generate plot
-def draw_fig_real(axes):
+# function to generate plot at each frame
+def draw_fig_real():
     #figure() # don't call, otherwise opens new window
     # plt.pyplot.imshow(z, interpolation='nearest')
     #plt.colorbar()
     #show()
+    plt.ion()
+    fig, axes1 = plt.subplots()
+    axes1.set_xlim(0,9)
     axes1.plot(X1,Y1)
 
-# video
-N = 16
-fig, axes1 = plt.pyplot.subplots()
-axes1.set_xlim(0,9)
+# for loop to create many frames
+N = 16 # number of frames
 for i in range(1,N):
     j=i*(len(X)//N)
     X1=X[0:j]
     Y1=Y[0:j]
-    drawnow(draw_fig_real)
+    draw_fig_real()
 
 
